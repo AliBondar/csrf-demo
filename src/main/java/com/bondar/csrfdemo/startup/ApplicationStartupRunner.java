589d6d698cc58e839d2ce3c1f5731f9ede17d79b
Ali Bondar <bondarali1380@gmail.com>
2025-05-22 13:02:35 +0330

    add runner class

--- src/main/java/com/bondar/csrfdemo/startup/ApplicationStartupRunner.java
+++ src/main/java/com/bondar/csrfdemo/startup/ApplicationStartupRunner.java
@@ -1,6 +1,6 @@
 package com.bondar.csrfdemo.startup;
 
-import lombok.extern.apachecommons.CommonsLog;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
